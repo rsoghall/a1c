b5701158e46fb087660e7b69f64e06b27906874d
Ryan <ryan@Ryans-MacBook-Pro.local>
2019-01-18 12:19:22 -0700

    Created CalResults component..added inbox box and Get A1c button to App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
-import axios from 'axios'
+// import axios from 'axios'
 import './App.css';
+// import CalcResults from './Components/CalcResults'
 
 class App extends Component {
+  constructor () {
+    super();
+  
+  // //Example
+  // // componentDidMount (){
+  // //   axios.get ('/api/a1c')
+  // //   .then(res => console.log(res))
 
-  //Example
-  componentDidMount (){
-    axios.get ('/api/a1c')
-    .then(res => console.log(res))
+  // handleResultInput(value){
+  //   this.setState({
+  //     result: value
+  //   })
   }
+  
+
   render() {
     return (
       <div className="App">
-        
+      <div>
+        <input ></input>
+        <button>Get A1c</button>
+      </div>
+      <h1>Input BG</h1>
+      <p>Calculate A1c from GB Input</p>
+      
       </div>
     );
   }
---
+++ src/Components/CalcResults.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const CalcResults = (props) => {
+    return (
+        <div>
+            <h1>Result {props.CalcResults.Result}</h1>
+        </div>
+    )
+}
+// export default CalcResults;
