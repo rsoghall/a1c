84db8be6d40dd82954231ae18e85d26659fa99bb
Ryan <ryan@Ryans-MacBook-Pro.local>
2019-01-19 12:24:22 -0700

    no changes that worked

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import CalcResults from './Components/CalcResults'
 import axios from 'axios';
+// import Input from './input'
 
 class App extends Component {
   constructor () {
@@ -23,6 +24,17 @@ class App extends Component {
       BG: value
     })
   }
+
+  // handleAddBG(){
+  //   this.state.result.push(this.state.BG)
+  //   var avg = (this.state.result.reduce((a, b) => parseFloat(a) + parseFloat(b), 0.0))/this.state.result.length
+
+  //   let A1c=(46.7 + +avg)/28.7
+  //   this.setState({
+  //     A1c: A1c,
+  //     BG: ''
+  //   })
+  // }
 
   handleAddBG(){
     let A1c=(46.7 + +this.state.BG)/28.7
@@ -67,16 +79,16 @@ class App extends Component {
     })
     return (
       <div className="App">
-      <div>
-        <input onChange = {(e) => this.handleInputBG(e.target.value)}
-        value={this.state.BG}
-        placeholder={'Add BG'}></input>
-        <button onClick={() =>this.handleAddBG()}>Get A1c</button>
-      </div>
-      {this.state.A1c}
-      {/* <h1>Input BG</h1>
-      <p>Calculate A1c from GB Input</p> */}
-      {mappedResults}
+      
+        <div>
+          <input onChange = {(e) => this.handleInputBG(e.target.value)}
+          value={this.state.BG}
+          placeholder={'Add BG'}/>
+          <button onClick={() =>this.handleAddBG()}>Get A1c</button>
+          <button onClick={() => this.handleGettingResults()}>Get Results</button>
+        </div>
+        {this.state.A1c}
+        {mappedResults}
       </div>
     );
   }
